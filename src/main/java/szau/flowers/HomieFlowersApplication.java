d469a07cdcc86cfe573a583604c999e475ad0a90
Pawel <pawelszalawinski@gmail.com>
2020-04-01 17:45:47 +0200

    initial commit

---
+++ src/main/java/szau/flowers/HomieFlowersApplication.java
@@ -0,0 +1,13 @@
+package szau.flowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomieFlowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomieFlowersApplication.class, args);
+	}
+
+}
